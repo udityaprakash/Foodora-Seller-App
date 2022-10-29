8669f3412ef15a901b9d6f09f12ea11eb0da0391
UDITYA PRAKASH <udityaprakash983@gmail.com>
2022-10-29 11:08:47 +0530

    started making homepage

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SafeArea(
+      theme: ThemeData(scaffoldBackgroundColor: Color.fromRGBO(35, 107, 150, 1
+      ),
+      textTheme: Typography().white,
+      ),
+      home: const SafeArea(
         child: Splashscreen()
         ),
     );
--- lib/screens/homepage/homepage.dart
+++ lib/screens/homepage/homepage.dart
@@ -5,6 +5,33 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container());
+    return Scaffold(body: Padding(
+      padding: const EdgeInsets.only(top:30.0,left: 20.0,right: 20.0),
+      child: Column(
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: const [
+              Text('Foodora',
+              style: TextStyle(
+                fontFamily: 'FoodoraFont',
+                fontSize: 55.0,
+              ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Text('for bussiness',              
+                style: TextStyle(
+                  fontFamily: 'FoodoraFont',
+                  fontSize: 25.0,
+                ),
+                ),
+              ),
+            ],
+            
+          )
+        ],
+      ),
+    ));
   }
 }
