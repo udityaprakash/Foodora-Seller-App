d826f185b258639a702a7de33e849977b9dc8121
UDITYA PRAKASH <udityaprakash983@gmail.com>
2022-11-05 20:17:51 +0530

    created responsive on landscape mode and add app icon in previous commit

--- lib/screens/homepage/homepage.dart
+++ lib/screens/homepage/homepage.dart
@@ -19,80 +19,80 @@ class Homepage extends StatelessWidget {
           child: Container(
             height: MediaQuery.of(context).size.height - 40.0,
             padding: EdgeInsets.symmetric(horizontal: 20.0),
-            child: Container(
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Container(
-                        // color: Colors.blue,
-                        width: 50,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(35.0),
-                          color: Color.fromRGBO(50, 81, 255, 1),
-                        ),
-                        child: SvgPicture.asset(
-                          'assets/svg/logo.svg',
-                          height: 50,
-                        ),
+            child: ListView(
+              shrinkWrap: true,
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      // color: Colors.blue,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(35.0),
+                        color: Color.fromRGBO(50, 81, 255, 1),
                       ),
-                    ],
-                  ),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left:25.0),
-                        child:
-                            textgenerator('Foodora', 50.0, 'FoodoraFont', 700,Colors.white),
+                      child: SvgPicture.asset(
+                        'assets/svg/logo.svg',
+                        height: 50,
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: textgenerator(
-                            'for bussiness', 22.0, 'FoodoraFont', 400,Colors.white),
-                      ),
-                    ],
-                  ),
-                  textgenerator('Take Your Restraunt Bussiness to Pinnacle', 16, 'Raleway', 800,Colors.white),
-                  const SizedBox(
-                    height: 25.0,
-                  ),
-                  svggenerator(
-                    350,
-                    350,
-                    'assets/svg/logo.svg',
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  buttongenerator('Sign In', context, () {
-                    Navigator.pushNamed(context, '/siginpage');
-                  }),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  buttongenerator('New Restraunt', context, () {
-                    Navigator.pushNamed(context, '/newregister');
-                  }),
-                  Expanded(
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: const [
-                        Text("Code Of Conduct",
-                            style: TextStyle(
-                                decoration: TextDecoration.underline)),
-                        Text("Terms Of Service",
-                            style: TextStyle(
-                                decoration: TextDecoration.underline)),
-                        Text("Privacy Policy",
-                            style:
-                                TextStyle(decoration: TextDecoration.underline))
-                      ],
                     ),
-                  )
-                ],
-              ),
+                  ],
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 25.0),
+                      child: textgenerator(
+                          'Foodora', 50.0, 'FoodoraFont', 700, Colors.white),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: textgenerator('for bussiness', 22.0, 'FoodoraFont',
+                          400, Colors.white),
+                    ),
+                  ],
+                ),
+                textgenerator('Take Your Restraunt Bussiness to Pinnacle', 16,
+                    'Raleway', 800, Colors.white),
+                const SizedBox(
+                  height: 25.0,
+                ),
+                svggenerator(
+                  350,
+                  350,
+                  'assets/svg/logo.svg',
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                buttongenerator('Sign In', context, () {
+                  Navigator.pushNamed(context, '/siginpage');
+                }),
+                const SizedBox(
+                  height: 30,
+                ),
+                buttongenerator('New Restraunt', context, () {
+                  Navigator.pushNamed(context, '/newregister');
+                }),
+                Expanded(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: const [
+                      Text("Code Of Conduct",
+                          style:
+                              TextStyle(decoration: TextDecoration.underline)),
+                      Text("Terms Of Service",
+                          style:
+                              TextStyle(decoration: TextDecoration.underline)),
+                      Text("Privacy Policy",
+                          style:
+                              TextStyle(decoration: TextDecoration.underline))
+                    ],
+                  ),
+                )
+              ],
             ),
           ),
         ),
