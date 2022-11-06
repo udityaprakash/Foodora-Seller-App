217cf063945d0f917889c94d7e9cc745be56343d
UDITYA PRAKASH <udityaprakash983@gmail.com>
2022-11-07 01:01:03 +0530

    created registerotp file dart

---
+++ lib/screens/newregister/registerotppage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Myotppage extends StatefulWidget {
+  const Myotppage({super.key});
+
+  @override
+  State<Myotppage> createState() => _MyotppageState();
+}
+
+class _MyotppageState extends State<Myotppage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
