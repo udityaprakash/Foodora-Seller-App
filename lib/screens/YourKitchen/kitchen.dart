e1f1b05161bed79a89ffcdc152b02d563ea0debd
udityaprakash <udityaprakash983@gmail.com>
2022-11-12 00:16:14 +0530

    main home page created for accepting the orders send by the sellers

--- lib/screens/YourKitchen/kitchen.dart
+++ lib/screens/YourKitchen/kitchen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:foodora_seller/screens/desigining.dart';
 
 class Kitchen extends StatefulWidget {
   const Kitchen({super.key});
@@ -10,6 +11,37 @@ class Kitchen extends StatefulWidget {
 class _KitchenState extends State<Kitchen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(51, 81, 255, 1),
+        centerTitle: true,
+        title: Text(
+          'Foodora',
+          style: TextStyle(
+              fontFamily: 'FoodoraFont',
+              fontSize: MediaQuery.of(context).size.width / 10),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.all(10),
+          // height: MediaQuery.of(context).size.height*0.7,
+          width: double.infinity,
+          // color: Colors.white,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+
+              textgenerator('Your Kitchen', MediaQuery.of(context).size.width/15, 'Raleway', 300, Color.fromRGBO(50, 81, 255, 1)),
+              Container(
+                margin: EdgeInsets.only(left:20,top: 20),
+                child: textgenerator('', 15, 'Raleway', 300, Colors.white),
+              ),
+              
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
